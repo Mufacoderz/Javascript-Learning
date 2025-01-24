f64ff253ecf5bcae89eb4ca8009e9bc2b2e4c035
Muhammad <mf9555759@gmail.com>
2025-01-24 19:38:31 +0800

    while

--- 2.1 - While/script.js
+++ 2.1 - While/script.js
@@ -1,20 +1,22 @@
-//var ulang = true;
-//while(ulang){
-//    console.log("hello word");
-//    ulang = confirm("lagi?");
-//}
+// var ulang = true;
+// while (ulang) {
+//     console.log("hello word"); // Menampilkan "hello word" di console.
+//     ulang = confirm("lagi?"); // Menampilkan dialog konfirmasi, jika "OK", ulang akan tetap true dan loop akan terus berjalan.
+// }
 
 
 
-var nilaiAwal = 1;
-while(nilaiAwal <= 5){
-    console.log("hello world");
-    nilaiAwal++;
+
+var nilaiAwal = 1; // Inisialisasi variabel nilai awal dengan 1.
+while (nilaiAwal <= 5) { // Loop berjalan selama nilaiAwal <= 5.
+    console.log("hello world"); // Menampilkan "hello world" di console.
+    nilaiAwal++; // Menambahkan 1 ke nilaiAwal di setiap iterasi.
 }
 
-var nilaiAwal = 1;
-while(nilaiAwal <= 5){
-    console.log("hello world" + nilaiAwal + "x");
-    nilaiAwal++;
+
+var nilaiAwal = 1; // Inisialisasi variabel nilai awal dengan 1.
+while (nilaiAwal <= 5) { // Loop berjalan selama nilaiAwal <= 5.
+    console.log("hello world " + nilaiAwal + "x"); // Menampilkan "hello world" diikuti nilaiAwal dan "x".
+    nilaiAwal++; // Menambahkan 1 ke nilaiAwal di setiap iterasi.
 }
 
